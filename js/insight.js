1f02e3518abee4137cffbeb78fe014e293b7eda5
ahtungko <itko1993@gmail.com>
2018-04-10 23:52:15 +0800

    change application insight to new address

--- js/insight.js
+++ js/insight.js
@@ -1,8 +1,7 @@
-
-  var appInsights=window.appInsights||function(a){
+var appInsights=window.appInsights||function(a){
     function b(a){c[a]=function(){var b=arguments;c.queue.push(function(){c[a].apply(c,b)})}}var c={config:a},d=document,e=window;setTimeout(function(){var b=d.createElement("script");b.src=a.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js",d.getElementsByTagName("script")[0].parentNode.appendChild(b)});try{c.cookie=d.cookie}catch(a){}c.queue=[];for(var f=["Event","Exception","Metric","PageView","Trace","Dependency"];f.length;)b("track"+f.pop());if(b("setAuthenticatedUserContext"),b("clearAuthenticatedUserContext"),b("startTrackEvent"),b("stopTrackEvent"),b("startTrackPage"),b("stopTrackPage"),b("flush"),!a.disableExceptionTracking){f="onerror",b("_"+f);var g=e[f];e[f]=function(a,b,d,e,h){var i=g&&g(a,b,d,e,h);return!0!==i&&c["_"+f](a,b,d,e,h),i}}return c
     }({
-        instrumentationKey:"8a7a849c-b547-4d18-97a6-10bc48720f69"
+        instrumentationKey:"956d0271-bb99-4a8f-be69-fb9513335bc4"
     });
     
   window.appInsights=appInsights,appInsights.queue&&0===appInsights.queue.length&&appInsights.trackPageView();
